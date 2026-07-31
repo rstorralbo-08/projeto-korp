e5ff659185d04d8132f871d19f462584270e0e52
Rodrigo Torralbo <rstorralbo@gmail.com>
2026-07-31 11:05:14 -0300

    Passo 1 - Etapas 1 e 2 - Testado localmente

---
+++ app/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Response struct {
+	Nome    string `json:"nome"`
+	Versao  string `json:"versao"`
+	Horario string `json:"horario"`
+}
+
+func projetoKorp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	response := Response{
+		Nome:    "Projeto Korp",
+		Versao:  "1.0",
+		Horario: time.Now().UTC().Format(time.RFC3339),
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/projeto-korp", projetoKorp)
+	log.Println("Servidor iniciado na porta 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
